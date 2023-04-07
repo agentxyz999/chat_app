68d6ef14d6ab6157ce3d2a7d08dd9dcedb3edf8d
Rolie Conde <onefivezero999@gmail.com>
2023-04-07 15:07:49 +0800

    styled the Chat/Home page and added the contacts component

---
+++ client/src/components/Contacts.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Logo from "../assets/logo.svg";
+
+const Contacts = ({ contacts, currentUser }) => {
+  const [currentUserName, setCurrentUserName] = useState(undefined);
+  const [currentUserImage, setCurrentUserImage] = useState(undefined);
+
+  useEffect(() => {}, [currentUser]);
+
+  return <div>Contacts</div>;
+};
+
+export default Contacts;
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { allUsersRoute } from "../utils/APIRoutes";
+import Contacts from "../components/Contacts";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const navigate = useNavigate();
+
+  //check if user is logged in by key in local storage
+  //if not redirect to login page
+  useEffect(() => {
+    const checkIsUserLoggedIn = async () => {
+      if (!localStorage.getItem("chat-app-user")) {
+        navigate("/login");
+      } else {
+        setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+      }
+    };
+    checkIsUserLoggedIn();
+  }, []);
+  //check if current user has set his/her avatar img and fetch contacts data from server
+  //if not redirect user to setAvatar page
+  useEffect(() => {
+    const checkUserAvatar = async () => {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data.data);
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    };
+    checkUserAvatar();
+  }, []);
+  return (
+    <Container>
+      <div className="container">
+        <Contacts contacts={contacts} currentUser={currentUser} />
+      </div>
+    </Container>
+  );
 };
 
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
 export default Chat;
