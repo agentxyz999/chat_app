8b1f377cde5376d610decdaa78f75b2340e62454
Rolie Conde <onefivezero999@gmail.com>
2023-04-17 11:34:07 +0800

    added welcome message to the user

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { allUsersRoute } from "../utils/APIRoutes";
 import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
 
 const Chat = () => {
   const [contacts, setContacts] = useState([]);
@@ -52,6 +53,7 @@ const Chat = () => {
           currentUser={currentUser}
           changeChat={handleChatChange}
         />
+        <Welcome currentUser={currentUser} />
       </div>
     </Container>
   );
